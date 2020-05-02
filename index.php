9c6197c38f80cefa6343810805c7d161951182af
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-05-02 17:41:50 -0500

    fixed it

--- index.php
+++ index.php
@@ -8,22 +8,20 @@ Author URI: https://github.com/0x416c616e
 Version: 0.1
 */
 add_action("admin_menu", "addMenu");
+
 function addMenu(){
-    add_menu_page("Dice Roller Menu", "Dice Roller Menu", 4, "example-options", "exampleMenu");
-    add_submenu_page("d6", "Roll D6", "Roll D6", 4, "roll-d6", "d6");
-    add_submenu_page("d20", "Roll D20", "Roll D20", 4, "roll-d20", "d20");
+    add_menu_page("Example Options", "Example Options", 4, "example-options", "d20");
+    add_submenu_page("example-options", "Option 1", "Option 1", 4, "example-option-1", "d6");
 }
 
-function exampleMenu(){
-    echo "You can roll dice here!";
+
+function d20(){
+    echo strval(rand(1, 20));
 }
 
 function d6() {
     echo strval(rand(1, 6));
 }
 
-function d20() {
-    echo strval(rand(1, 20));
-}
 
 ?>
